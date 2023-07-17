57e4867e4e5fed46df44d365863fd5cadce4d306
paulgo <spam@heropaul.com>
2023-07-17 09:52:53 -0400

    Initial

---
+++ HtmlReelsSubscribe.ts
@@ -0,0 +1,12 @@
+
+namespace Reels
+{
+	/** */
+	export function subscribe(...feedUrls: string[])
+	{
+		
+	}
+}
+
+//@ts-ignore
+if (typeof module === "object") Object.assign(module.exports, { Reels });
